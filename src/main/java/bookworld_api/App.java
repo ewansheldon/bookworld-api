500aa79488ba7666e679b0655c0d0e1626b1d126
Ewan <ewan.s@cucumberwifi.io>
2020-06-25 11:25:06 +0100

    optimises imports and reformats code

--- src/main/java/bookworld_api/App.java
+++ src/main/java/bookworld_api/App.java
@@ -1,50 +1,50 @@
 package bookworld_api;
 
-import com.google.gson.Gson;
-import spark.Filter;
-import spark.Spark;
-
-import java.util.HashMap;
-import java.util.List;
-
 import static spark.Spark.after;
 import static spark.Spark.get;
 
+import com.google.gson.Gson;
+import java.util.HashMap;
+import java.util.List;
+import spark.Filter;
+import spark.Spark;
+
 public class App {
 
-    public static final int DEFAULT_PORT = 8080;
+  public static final int DEFAULT_PORT = 8080;
 
-    public static void main(String[] strings) {
-        Spark.port(getPort());
+  public static void main(String[] strings) {
+    Spark.port(getPort());
 
-        after((Filter) (request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
-            response.header("Access-Control-Allow-Methods", "GET");
-        });
+    after((Filter) (request, response) -> {
+      response.header("Access-Control-Allow-Origin", "*");
+      response.header("Access-Control-Allow-Methods", "GET");
+    });
 
-        get("books/:country_code", (req, res) -> {
-            res.type("application/json");
-            return new Gson().toJson(bookObject());
-        });
+    get("books/:country_code", (req, res) -> {
+      res.type("application/json");
+      return new Gson().toJson(bookObject());
+    });
 
-        get("countries", (req, res) -> {
-            res.type("application/json");
-            return new Gson().toJson(List.of("GBR", "PRT", "AUS"));
-        });
-    }
+    get("countries", (req, res) -> {
+      res.type("application/json");
+      return new Gson().toJson(List.of("GBR", "PRT", "AUS"));
+    });
+  }
 
-    private static Object bookObject() {
-        HashMap<String, String> res = new HashMap();
-        res.put("title", "Vile Bodies");
-        res.put("author", "Evelyn Waugh");
-        res.put("publication_date", "1930");
-        return res;
-    }
+  private static Object bookObject() {
+    HashMap<String, String> res = new HashMap();
+    res.put("title", "Vile Bodies");
+    res.put("author", "Evelyn Waugh");
+    res.put("publication_date", "1930");
+    return res;
+  }
 
-    private static int getPort() {
-        String port = System.getenv("PORT");
-        if (port != null)
-            return Integer.parseInt(port);
-        return DEFAULT_PORT;
-    }
+  private static int getPort() {
+    String port = System.getenv("PORT");
+      if (port != null) {
+          return Integer.parseInt(port);
+      }
+    return DEFAULT_PORT;
+  }
 }
--- src/test/java/bookworld_api/APITest.java
+++ src/test/java/bookworld_api/APITest.java
@@ -1,31 +1,32 @@
 package bookworld_api;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
 class APITest {
 
-    @BeforeAll
-    static void beforeAll() {
-        App.main(new String[0]);
-    }
+  @BeforeAll
+  static void beforeAll() {
+    App.main(new String[0]);
+  }
 
-    @Test
-    void should_get_book_from_country_code_endpoint() {
-        given().port(8080).when().request("GET", "/books/GBR")
-                .then().statusCode(200)
-                .assertThat().body("title", equalTo("Vile Bodies"))
-                .assertThat().body("author", equalTo("Evelyn Waugh"))
-                .assertThat().body("publication_date", equalTo("1930"));
-    }
+  @Test
+  void should_get_book_from_country_code_endpoint() {
+    given().port(8080).when().request("GET", "/books/GBR")
+        .then().statusCode(200)
+        .assertThat().body("title", equalTo("Vile Bodies"))
+        .assertThat().body("author", equalTo("Evelyn Waugh"))
+        .assertThat().body("publication_date", equalTo("1930"));
+  }
 
-    @Test
-    void should_get_countries_where_books_are_from() {
-        given().port(8080).when().request("GET", "/countries")
-                .then().statusCode(200)
-                .assertThat().body("", hasItems("GBR", "PRT", "AUS"));
-    }
+  @Test
+  void should_get_countries_where_books_are_from() {
+    given().port(8080).when().request("GET", "/countries")
+        .then().statusCode(200)
+        .assertThat().body("", hasItems("GBR", "PRT", "AUS"));
+  }
 }
